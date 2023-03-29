7a9cb5bbd02a358a70ff9ee68df4e0f651ef8048
DarthTekin <tekinteks@gmail.com>
2023-03-29 16:46:34 +0300

    Bool variable initialization tested; Contract deployed.

--- contracts/Booleans.sol
+++ contracts/Booleans.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.19;
 
 contract Booleans{
-    bool public isOwner;
+    bool public isOwner = true;
 
     function getVar() public view returns (bool){
         return isOwner;
