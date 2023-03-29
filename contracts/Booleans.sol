19a779dd206cd737d509977e3abdbd7046d4a9d9
DarthTekin <tekinteks@gmail.com>
2023-03-29 16:40:10 +0300

    Bool variable initialization tested; Contract deployed.

---
+++ contracts/Booleans.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract Booleans{
+    bool public isOwner;
+
+    function getVar() public view returns (bool){
+        return isOwner;
+    }
+}
