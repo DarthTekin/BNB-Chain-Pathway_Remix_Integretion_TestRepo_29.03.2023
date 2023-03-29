2155a41ecb86a2b3b4e503d6327b898da939149a
DarthTekin <tekinteks@gmail.com>
2023-03-29 23:36:33 +0300

    String and Bytes type of variables added and converted.

--- contracts/StringAndBytes.sol
+++ contracts/StringAndBytes.sol
@@ -3,5 +3,9 @@
 pragma solidity ^0.8.19;
 
 contract StringAndBytes{
+    string public greeting = "Greetings!";
 
+    bytes public helloText = "Hello, World!";
+
+    string public convertedText = string(helloText);
 }
