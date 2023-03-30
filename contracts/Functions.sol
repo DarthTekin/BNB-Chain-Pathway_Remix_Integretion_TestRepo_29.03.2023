88ae910c4cc7428b00b44e2f96e2fd78df8024ae
DarthTekin <tekinteks@gmail.com>
2023-03-31 02:16:29 +0300

    viewExample() view function added and deployed.

--- contracts/Functions.sol
+++ contracts/Functions.sol
@@ -3,5 +3,9 @@
 pragma solidity ^0.8.19;
 
 contract Functions{
-    
+    string public stateString = "Hello World";
+
+    function viewExample() public view returns(string memory){
+        return stateString;
+    }
 }
