794507230f3bc0dfc81e0bdff7621f121fdf6c38
DarthTekin <tekinteks@gmail.com>
2023-03-31 17:35:53 +0300

    constructor() function installed to initialize state variables.

--- contracts/ConstructorExample.sol
+++ contracts/ConstructorExample.sol
@@ -6,4 +6,10 @@ contract ConstructorExample{
     address public owner;
     string public name;
     uint public age;
+
+    constructor(string memory _name, uint _age){
+        owner = msg.sender;
+        name = _name;
+        age = _age;
+    }
 }
