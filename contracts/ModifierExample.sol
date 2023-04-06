85606b2dea1358f335f76bd6911a5b3f4f2cf1bf
DarthTekin <tekinteks@gmail.com>
2023-04-06 16:15:45 +0300

    ModifierExample.sol created and deployed.

---
+++ contracts/ModifierExample.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract ModifierExample{
+    address public owner;
+    uint public myNumber;
+
+    constructor(uint _myNumber){
+        owner = msg.sender;
+        myNumber = _myNumber;
+    }
+
+    modifier onlyOwnerOrAddress(address _allowedAddress){
+        require(msg.sender == owner || msg.sender == _allowedAddress, "Only the owner or an allowed addess can call this function");
+        _;
+    }
+
+    function changeNumber(uint _newNumber) public onlyOwnerOrAddress(address(0x456)){
+        myNumber = _newNumber;
+    }
+}
